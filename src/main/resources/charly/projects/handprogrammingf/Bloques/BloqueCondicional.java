afe31c8560247f9ad44952a3ced66f0343341e22
juuzojason <juan.tobijose@gmail.com>
2024-12-03 16:00:35 -0500

    PUSH

---
+++ src/main/resources/charly/projects/handprogrammingf/Bloques/BloqueCondicional.java
@@ -0,0 +1,95 @@
+
+package Bloques;
+
+import Model.ConectorMultiple;
+import Model.EvaluadorExpresiones;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+
+public class BloqueCondicional extends BloqueEjecutable{
+    
+    private StackPane stackPane;
+    private Label label;
+    private String Text;
+    
+    
+    public BloqueCondicional(double x, double y, String Text, Color c) {
+        super(x, y);
+        if (Text == null || Text.trim().isEmpty()) {
+            throw new IllegalArgumentException("El texto del bloque no puede ser nulo o vacío.");
+        }
+        this.Text = Text;
+        this.ColorBloque = c;
+        IniciarComponentes();
+        Pintar();
+    }
+
+    
+    /*
+    Crear los componentes visuales necesarios para representar un bloque condicional.
+    Esto incluye la creación de un StackPane que contiene una etiqueta con el texto 
+    del bloque y la inicialización de un conector múltiple (cvertical) que parece estar relacionado con la lógica de conexión del bloque.
+    */
+    @Override
+    public void IniciarComponentes() {
+        super.IniciarComponentes();
+        
+        this.cvertical = new ConectorMultiple(this);
+        
+        
+        stackPane = new StackPane();
+        stackPane.setAlignment(Pos.CENTER_RIGHT);
+
+        label = new Label(Text);
+
+        Font font = new Font("Berlin Sans FB", 35);
+        label.setFont(font);
+
+        stackPane.getChildren().add(label);
+        getChildren().add(stackPane);
+    }
+
+    
+    /*
+    Calcula el ancho necesario para mostrar un texto con una fuente específica
+    Esto puede ser útil para ajustar el tamaño de los elementos que contienen el texto, como el StackPan
+    para asegurarse de que el texto encaje adecuadamente sin ser recortado ni desbordado. 
+    */
+    private double computeTextWidth(javafx.scene.text.Font font, String text) {
+        javafx.scene.text.Text textNode = new javafx.scene.text.Text(text);
+        textNode.setFont(font);
+        return textNode.getBoundsInLocal().getWidth();
+    }
+
+
+
+    /*
+     Se encarga de establecer el tamaño visual (ancho y alto) de un bloque condicional y ajustar su contenido 
+    (en este caso, un StackPane que contiene una etiqueta de texto) para que se muestre correctamente en la interfaz gráfica.
+    */
+    @Override
+    public void Pintar() {
+        super.Pintar();
+        double textWidth = computeTextWidth(label.getFont(), Text);
+
+        stackPane.setPrefWidth(textWidth + 30);
+        stackPane.setPrefHeight(65);
+        setAncho(textWidth + 62);
+        label.setVisible(true);
+    }
+    
+    
+    //Evalua el bloque que le sigue
+    public boolean evaluarSiguiente(){
+        return EvaluadorExpresiones.EvCondicion(Siguiente());
+    }
+    
+    
+    
+    
+    
+}
---
+++ src/main/resources/charly/projects/handprogrammingf/Bloques/BloqueEjecutable.java
@@ -0,0 +1,131 @@
+
+package Bloques;
+
+import Model.Bloque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import javafx.scene.paint.Color;
+
+
+public class BloqueEjecutable extends Bloque{
+    public HashMap<String,String> variables = new HashMap<>();
+    
+    
+    public BloqueEjecutable(double x, double y) {
+        super(x, y);
+
+        Inconectableh = true;
+    }
+
+    /*
+     Verifica si una variable con un nombre específico existe en la lista de variables 
+    del objeto BloqueEjecutable y devuelve true si existe y false si no existe. 
+    */
+    public boolean esVariable(String nombre) {
+        for (String variable : variables.keySet()) {
+            if (variable.equals(nombre)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+ 
+    //permite obtener el valor de una variable específica buscándola por nombre en la lista de variables del objeto BloqueEjecutable
+    public String getValor(BloqueVariable b) {
+        if (esVariable(b.getNombre())){
+            return variables.get(b.getNombre());
+        }
+//        for (BloqueVariable variable : variables) {
+//            if (variable.getNombre().equals(b.getNombre())) {
+//                return variable.getValor();
+//            }
+//        }
+        
+        return "Non";
+    }
+    
+    
+    /*
+    permite establecer o modificar el valor de una variable específica en la lista de variables del objeto BloqueEjecutable.
+    Si la variable con el mismo nombre ya existe en la lista
+    Actualiza su valor; de lo contrario, agrega una nueva variable a la lista con el nombre y valor especificados
+    */
+    public void setValor(BloqueVariable b, String valor) {
+        if (b == null || b.getNombre() == null || valor == null) {
+            throw new IllegalArgumentException("BloqueVariable y su valor no pueden ser nulos.");
+        }
+        variables.put(b.getNombre(), valor);
+        System.out.println(variables.toString());
+//        for (BloqueVariable variable : variables) {
+//            if (variable.getNombre().equals(b.getNombre())) {
+//                variable.setValor(valor);
+//                hacambiado = true;
+//            } 
+//        }
+        if (ejecutador != null){
+            if (ejecutador.esVariable(b.getNombre())){
+                ejecutador.setValor(b, valor);
+            }
+        }
+
+    }
+    
+    
+    public void deleteValor(BloqueVariable b){
+        variables.remove(b.getNombre());
+    }
+    
+    
+    
+    //Hasta ahora elimina todas las variables almacenadas en la lista variables de un objeto BloqueEjecutable
+    public void vaciarVariables() {
+        variables.clear();
+        limpiarEjecutadores();
+        
+        //Ejecutar la siguiente linea
+        if (!(this instanceof BloqueInicio)){
+            Bloque sig = SiguienteLinea();
+            while (sig != null || sig instanceof BloqueElif || sig instanceof BloqueElse){
+                sig = sig.SiguienteLinea();
+            }
+            if (sig != null) {
+                sig.ejecutador = this.ejecutador;
+                sig.Hacer();
+            }
+        }
+    }
+    
+    public void limpiarEjecutadores(){
+    }
+    
+    
+    
+    
+    /*
+    realiza acciones relacionadas con la manipulación de variables y la ejecución de bloques condicionales. 
+    El comportamiento exacto dependerá de la implementación de las clases y
+    objetos específicos que utilizan esta función en el código completo.
+    */
+    @Override
+    public void Hacer(){
+        this.LineaEjecutador();
+        if (ejecutador.variables !=  null) variables.putAll(ejecutador.variables);
+        if (this instanceof BloqueCondicional && !(this instanceof BloqueFor)) EjecutarHijos();
+    }
+    
+    /*
+     se utiliza para ejecutar los bloques hijos de un bloque condicional en ciertas condiciones.
+    Verifica si existe una conexión (conexion) con un bloque hijo y, si es el caso,
+    configura el objeto actual como el ejecutador del bloque hijo y ejecuta dicho bloque. 
+    */
+    public void EjecutarHijos(){
+        if (this.cvertical.inner.conexion == null) return;
+        this.cvertical.inner.conexion.ejecutador = this;
+        this.cvertical.inner.conexion.Hacer();
+    }
+    
+    
+    
+}
